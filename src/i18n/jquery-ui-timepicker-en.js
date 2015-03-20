35e087c767f0c47e67500e4d185f98725e8b1ecf
Kostiantyn Tsaregradskyi <tsaregradskyi@gmail.com>
2015-03-20 12:37:36 +0200

    Create jquery-ui-timepicker-en.js

---
+++ src/i18n/jquery-ui-timepicker-en.js
@@ -0,0 +1,21 @@
+/* English translation for the jQuery Timepicker Addon */
+/* Extracted from main file for unification purposes */
+(function($) {
+	$.timepicker.regional['de'] = {
+		timeOnlyTitle: 'Choose Time',
+		timeText: 'Time',
+		hourText: 'Hour',
+		minuteText: 'Minute',
+		secondText: 'Second',
+		millisecText: 'Millisecond',
+		microsecText: 'Microsecond',
+		timezoneText: 'Time Zone',
+		currentText: 'Now',
+		closeText: 'Done',
+		timeFormat: 'HH:mm',
+		amNames: ['AM', 'A'],
+		pmNames: ['PM', 'P'],
+		isRTL: false
+	};
+	$.timepicker.setDefaults($.timepicker.regional['de']);
+})(jQuery);	
