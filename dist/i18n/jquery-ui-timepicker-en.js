67d0dc58cc108dc899064ab4d2df92249137fa66
Sergey Prilukin <sprilukin@gmail.com>
2015-11-23 14:07:57 +0200

    added 'en' i18n translation bundle

---
+++ dist/i18n/jquery-ui-timepicker-en.js
@@ -0,0 +1,23 @@
+/* Spanish translation for the jQuery Timepicker Addon */
+/* Written by Ianaré Sévi */
+/* Modified by Carlos Martínez */
+(function($) {
+	$.timepicker.regional['en'] = {
+		timeOnlyTitle: 'Choose Time',
+		timeText: 'Time',
+		hourText: 'Hour',
+		minuteText: 'Minute',
+		secondText: 'Second',
+		millisecText: 'Millisecond',
+		microsecText: 'Microsecond',
+		timezoneText: 'Time Zone',
+		currentText: 'Now',
+		closeText: 'Done',
+		timeFormat: 'HH:mm',
+		timeSuffix: '',
+		amNames: ['AM', 'A'],
+		pmNames: ['PM', 'P'],
+		isRTL: false
+	};
+	$.timepicker.setDefaults($.timepicker.regional['en']);
+})(jQuery);
